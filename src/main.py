d68f62d6e8943a062bc544b1ddd0d8e6bb560c43
Jeffrey Bakker <jeffrey.bakker@gmail.com>
2021-12-15 21:14:00 +0100

    add middleware for CORS headers

--- src/main.py
+++ src/main.py
@@ -1,8 +1,26 @@
+import os
+
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from routers import events, albums
 
 api = FastAPI()
 
+# Add CORS headers to the responses of the API
+origins = {
+    'production': [],
+    'staging': [],
+    'development': ['http://localhost:3000'],
+}[os.getenv('ENVIRONMENT', 'production')]
+api.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"] if os.getenv('ENVIRONMENT', 'production') == 'development' else []
+)
+
+# Include routers
 api.include_router(events.router)
 api.include_router(albums.router)
