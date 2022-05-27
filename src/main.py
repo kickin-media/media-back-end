627d593957588b65c4aaa9d78bf55d054c907610
Jonathan Juursema <jonathanjuursema@users.noreply.github.com>
2022-05-27 17:40:11 +0200

    Process photos asynch via Lambda+SQS (#4)

--- src/main.py
+++ src/main.py
@@ -2,12 +2,10 @@ import os
 
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from fastapi_utils.tasks import repeat_every
 
 import variables
 from routers import events, albums, photos, author
 
-from tasks.process_uploaded_photo import task as process_uploaded_photo
 
 app_environment = os.getenv('ENVIRONMENT', None)
 if app_environment is None:
@@ -32,10 +30,3 @@ api.include_router(events.router)
 api.include_router(albums.router)
 api.include_router(photos.router)
 api.include_router(author.router)
-
-
-# Set-up tasks
-@api.on_event('startup')
-@repeat_every(seconds=30, wait_first=False, raise_exceptions=True)
-def process_uploaded_photo_task():
-    process_uploaded_photo()
