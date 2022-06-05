89806321dcd214cb376c670c3ca0c3d472f8c60f
Jonathan Juursema <github@jonathanj.nl>
2022-06-05 14:38:44 +0200

    Add endpoints for managing event watermarks. (Fixes #7)

--- src/routers/events.py
+++ src/routers/events.py
@@ -7,8 +7,12 @@ from typing import List
 
 from models.event import Event, EventCreate, EventReadList, EventReadSingle
 from models.album import AlbumReadList
+from models.photo import PhotoUploadPreSignedUrl
+
+from variables import S3_BUCKET_ASSET_PATH, S3_UPLOAD_EXPIRY, S3_BUCKET, S3_PHOTO_HOSTNAME
 
 import uuid
+import boto3
 
 router = APIRouter(
     prefix="/event",
@@ -32,7 +36,7 @@ async def get_event(event_id: str, db: Session = Depends(get_db)):
 
 @router.get("/{event_id}/albums", response_model=List[AlbumReadList])
 async def get_event_albums(event_id: str, db: Session = Depends(get_db),
-                                         auth_data=Depends(JWTBearer(auto_error=False))):
+                           auth_data=Depends(JWTBearer(auto_error=False))):
     include_hidden = auth_data and 'albums:read_hidden' in auth_data['permissions']
 
     event = db.get(Event, event_id)
@@ -94,3 +98,53 @@ async def delete_event(event_id: str, db: Session = Depends(get_db)):
     db.commit()
 
     raise HTTPException(status_code=200, detail="event_deleted")
+
+
+# WATERMARKS
+
+
+@router.get("/{event_id}/watermark")
+async def get_event_watermark(event_id: str, db: Session = Depends(get_db)):
+    event = db.get(Event, event_id)
+    if event is None:
+        raise HTTPException(status_code=404, detail="event_not_found")
+
+    watermark_s3_url = "/".join([
+        S3_PHOTO_HOSTNAME,
+        "/".join([S3_BUCKET_ASSET_PATH, "watermark-{}.png".format(event.id)])
+    ])
+
+    return watermark_s3_url
+
+
+@router.post("/{event_id}/watermark", response_model=PhotoUploadPreSignedUrl,
+             dependencies=[Depends(JWTBearer(required_permissions=['events:manage']))])
+async def create_event_watermark_upload(event_id: str,
+                                        db: Session = Depends(get_db)):
+    event = db.get(Event, event_id)
+    if event is None:
+        raise HTTPException(status_code=404, detail="event_not_found")
+
+    upload_url = boto3.client('s3').generate_presigned_post(
+        S3_BUCKET,
+        "/".join([S3_BUCKET_ASSET_PATH, "watermark-{}.png".format(event.id)]),
+        ExpiresIn=S3_UPLOAD_EXPIRY
+    )
+
+    return upload_url
+
+
+@router.delete("/{event_id}/watermark", response_model=PhotoUploadPreSignedUrl,
+               dependencies=[Depends(JWTBearer(required_permissions=['events:manage']))])
+async def delete_event_watermark(event_id: str,
+                                 db: Session = Depends(get_db)):
+    event = db.get(Event, event_id)
+    if event is None:
+        raise HTTPException(status_code=404, detail="event_not_found")
+
+    boto3.client('s3').delete_object(
+        Bucket=S3_BUCKET,
+        Key="/".join([S3_BUCKET_ASSET_PATH, "watermark-{}.png".format(event.id)])
+    )
+
+    raise HTTPException(status_code=200, detail="watermark_deleted")
--- src/variables.py
+++ src/variables.py
@@ -24,6 +24,7 @@ S3_BUCKET = os.getenv('S3_PHOTO_BUCKET')
 S3_BUCKET_UPLOAD_PATH = "uploads"
 S3_BUCKET_ORIGINAL_PATH = "originals"
 S3_BUCKET_PHOTO_PATH = "photos"
+S3_BUCKET_ASSET_PATH = "assets"
 S3_UPLOAD_EXPIRY = 15 * 60  # Upload link is valid for 15 minutes.
 S3_PHOTO_HOSTNAME = os.getenv('S3_PHOTO_HOSTNAME', 'http://replace.me')
 
