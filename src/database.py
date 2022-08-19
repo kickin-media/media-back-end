d83f0108829b8b79e0e58a1dbedb90bc938ee2e6
Jonathan Juursema <jonathan.juursema@nedap.com>
2022-08-19 17:26:19 +0200

    Fix databases issues. (Fixes #16)

--- src/database.py
+++ src/database.py
@@ -1,9 +1,10 @@
 from sqlmodel import create_engine as create_sqlmodel_engine
 from sqlmodel import Session
+from sqlmodel.sql.expression import Select, SelectOfScalar
 
 from variables import DB_CONNECTION_STRING
 
-engine = create_sqlmodel_engine(DB_CONNECTION_STRING)
+engine = create_sqlmodel_engine(DB_CONNECTION_STRING, pool_recycle=540)
 
 db_session = Session(engine)
 
@@ -11,3 +12,7 @@ db_session = Session(engine)
 def get_db():
     with Session(engine) as session:
         yield session
+
+# From https://github.com/tiangolo/sqlmodel/issues/189#issuecomment-1025190094
+SelectOfScalar.inherit_cache = True  # type: ignore
+Select.inherit_cache = True  # type: ignore
