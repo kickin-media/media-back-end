2c2ec0d82ae4dd4587a148dfef222ba5e6b1627a
Jonathan Juursema <jonathanjuursema@users.noreply.github.com>
2023-03-28 21:37:18 +0200

    Add requirement for compiling Pillow. (#42)

--- terraform/main.tf
+++ terraform/main.tf
@@ -14,6 +14,10 @@ terraform {
       source  = "hashicorp/aws"
       version = ">= 4.40.0"
     }
+    klayers = {
+      version = "~> 1.0.1"
+      source  = "ldcorentin/klayer"
+    }
   }
 
 }
@@ -25,4 +29,10 @@ provider "aws" {
 provider "aws" {
   region = "us-east-1"
   alias  = "aws-global"
+}
+
+data "klayers_package_latest_version" "pillow" {
+  name   = "Pillow"
+  region = "eu-west-1"
+  python_version = "3.8"
 }
