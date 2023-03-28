d96659e01d5dee389e0cb9cc065beeac189b23e0
Jonathan Juursema <jonathan.juursema@nedap.com>
2023-03-28 20:34:38 +0200

    Add requirement for compiling Pillow.

--- terraform/modules/photo-processing/lambda.tf
+++ terraform/modules/photo-processing/lambda.tf
@@ -6,9 +6,13 @@ resource "aws_lambda_function" "processing_lambda" {
   function_name = "photo-processing-lambda-${var.stage}"
   role          = aws_iam_role.lambda-execution-role.arn
   handler       = "index.process"
-  runtime       = "python3.9"
+  runtime       = "python3.8"
   memory_size   = 2048
   timeout       = 30
+
+  layers = [
+    var.pillow_layer_arn
+  ]
 
   filename         = local.lambda_location
   source_code_hash = data.archive_file.processing_lambda_source.output_base64sha256
--- terraform/modules/photo-processing/variables.tf
+++ terraform/modules/photo-processing/variables.tf
@@ -7,3 +7,8 @@ variable "photo_bucket_arn" {
   description = "ARN for the S3 bucket where the processing gets and puts is photos."
   type        = string
 }
+
+variable "pillow_layer_arn" {
+  description = "The layer ARN for the Lambda layer containing Pillow."
+  type        = string
+}
