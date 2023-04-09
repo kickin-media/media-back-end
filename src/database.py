6918fc8bfba913800fee02d765c3beae5fa3c4a4
Jonathan Juursema <jonathan.juursema@nedap.com>
2023-04-09 14:47:11 +0200

    Disable connection pooling on API, because of server limitations.

--- src/database.py
+++ src/database.py
@@ -1,10 +1,11 @@
+import sqlalchemy.pool
 from sqlmodel import create_engine as create_sqlmodel_engine
 from sqlmodel import Session
 from sqlmodel.sql.expression import Select, SelectOfScalar
 
 from variables import DB_CONNECTION_STRING
 
-engine = create_sqlmodel_engine(DB_CONNECTION_STRING, pool_recycle=540)
+engine = create_sqlmodel_engine(DB_CONNECTION_STRING, poolclass=sqlalchemy.pool.NullPool)
 
 db_session = Session(engine)
 
@@ -12,6 +13,7 @@ db_session = Session(engine)
 def get_db():
     with Session(engine) as session:
         yield session
+
 
 # From https://github.com/tiangolo/sqlmodel/issues/189#issuecomment-1025190094
 SelectOfScalar.inherit_cache = True  # type: ignore
