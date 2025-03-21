be6448532ab5b98a7e0d2149358d0c6e4cde4367
Jonathan Juursema <jonathan.juursema@nedap.com>
2025-03-21 16:34:09 +0100

    Fix dummy certificate ARNs.

--- terraform/environments/bata/variables.tf
+++ terraform/environments/bata/variables.tf
@@ -13,14 +13,16 @@ variable "stages" {
       "base_domain" : "dev.fotos.batavierenrace.nl",
       "extra_cors_hostnames" : ["http://localhost", "http://localhost:3000"]
       "photo_bucket_name" : "bata-media-photo-dev"
-      "certificate_arn" : "arn:aws:acm:us-east-1:xxx:certificate/xxx"
+      # The certificate ARN still needs to be updated after they are created.
+      "certificate_arn" : "arn:aws:acm:us-east-1:172483268424:certificate/85a04c7d-efec-4f18-ad2c-f16f9edb27de"
     },
     "prod" : {
       "name" : "prod",
       "base_domain" : "fotos.batavierenrace.nl",
       "extra_cors_hostnames" : []
       "photo_bucket_name" : "bata-media-photo-prod"
-      "certificate_arn" : "arn:aws:acm:us-east-1:xxx:certificate/xxx"
+      # The certificate ARN still needs to be updated after they are created.
+      "certificate_arn" : "arn:aws:acm:us-east-1:172483268424:certificate/9000e290-e8b6-448c-a1e6-6da09a58ee2b"
     }
   }
 }
