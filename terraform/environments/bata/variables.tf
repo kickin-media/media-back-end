a729475862c5697f89d20da3f724e21847147397
Jonathan Juursema <jonathan.juursema@nedap.com>
2025-03-28 21:14:03 +0100

    Update ACM ARN.

--- terraform/environments/bata/variables.tf
+++ terraform/environments/bata/variables.tf
@@ -10,19 +10,17 @@ variable "stages" {
   default     = {
     "dev" : {
       "name" : "dev",
-      "base_domain" : "dev.fotos.batavierenrace.nl",
+      "base_domain" : "dev.foto.batavierenrace.nl",
       "extra_cors_hostnames" : ["http://localhost", "http://localhost:3000"]
       "photo_bucket_name" : "bata-media-photo-dev"
-      # The certificate ARN still needs to be updated after they are created.
-      "certificate_arn" : "arn:aws:acm:us-east-1:172483268424:certificate/85a04c7d-efec-4f18-ad2c-f16f9edb27de"
+      "certificate_arn" : "arn:aws:acm:us-east-1:172483268424:certificate/c8660770-d8c2-4976-9d95-145289b45c1e"
     },
     "prod" : {
       "name" : "prod",
-      "base_domain" : "fotos.batavierenrace.nl",
+      "base_domain" : "foto.batavierenrace.nl",
       "extra_cors_hostnames" : []
       "photo_bucket_name" : "bata-media-photo-prod"
-      # The certificate ARN still needs to be updated after they are created.
-      "certificate_arn" : "arn:aws:acm:us-east-1:172483268424:certificate/9000e290-e8b6-448c-a1e6-6da09a58ee2b"
+      "certificate_arn" : "arn:aws:acm:us-east-1:172483268424:certificate/411d3e09-0a4e-4ed1-b73d-9c7e69d9d2da"
     }
   }
 }
