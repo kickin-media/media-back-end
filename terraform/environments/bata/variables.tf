de3a3e0c81f3f80daa3058783f985b35667a9e83
Jonathan Juursema <jonathan.juursema@nedap.com>
2025-03-30 15:44:53 +0200

    No dev needed for Bata.

--- terraform/environments/bata/variables.tf
+++ terraform/environments/bata/variables.tf
@@ -8,13 +8,6 @@ variable "stages" {
   description = "The stages this application has"
   type        = map
   default     = {
-    "dev" : {
-      "name" : "dev",
-      "base_domain" : "dev.foto.batavierenrace.nl",
-      "extra_cors_hostnames" : ["http://localhost", "http://localhost:3000"]
-      "photo_bucket_name" : "bata-media-photo-dev"
-      "certificate_arn" : "arn:aws:acm:us-east-1:172483268424:certificate/c8660770-d8c2-4976-9d95-145289b45c1e"
-    },
     "prod" : {
       "name" : "prod",
       "base_domain" : "foto.batavierenrace.nl",
