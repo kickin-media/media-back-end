c04ee450f777e1001c2f1ab30bf3f943a7b5b9db
Jonathan Juursema <jonathanjuursema@users.noreply.github.com>
2025-04-25 11:29:33 +0200

    Change GHA-AWS authentication from keys to OIDC. (#187)

--- terraform/modules/frontend-s3-bucket/variables.tf
+++ terraform/modules/frontend-s3-bucket/variables.tf
@@ -5,10 +5,16 @@ variable "frontend_bucket_name" {
 
 variable "frontend_bucket_hostnames" {
   description = "The hostnames the bucket will be reachable on."
-  type        = list(string)
+  type = list(string)
 }
 
 variable "bucket_acm_arn" {
   description = "The ACM ARN of the certificate that will be used for TLS on this bucket."
   type        = string
+}
+
+variable "frontend_workflow_repo" {
+  description = "The GitHub repository in which the workflow lives that publishes the frontend."
+  type        = string
+  default     = "kickin-media/media-front-end"
 }
