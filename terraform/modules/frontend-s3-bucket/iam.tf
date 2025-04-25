f5f214dcbf734f075d92b51357acd85cada6fce5
Jonathan Juursema <jonathan.juursema@nedap.com>
2025-04-25 10:59:38 +0200

    Change GHA-AWS authentication from keys to OIDC.

--- terraform/modules/frontend-s3-bucket/iam.tf
+++ terraform/modules/frontend-s3-bucket/iam.tf
@@ -1,21 +1,12 @@
-resource "aws_iam_user" "upload_user" {
-  name = "${aws_s3_bucket.frontend_s3_bucket.bucket}-upload-user"
-}
-
-resource "aws_iam_user_policy_attachment" "upload_user_attachment" {
-  policy_arn = aws_iam_policy.upload_user_policy.arn
-  user       = aws_iam_user.upload_user.name
-}
-
-resource "aws_iam_policy" "upload_user_policy" {
+resource "aws_iam_policy" "upload_role_policy" {
   name = "${aws_s3_bucket.frontend_s3_bucket.bucket}-upload-policy"
   policy = jsonencode({
-    Version   = "2012-10-17"
+    Version = "2012-10-17"
     Statement = [
       {
         Effect   = "Allow",
         Resource = "${aws_s3_bucket.frontend_s3_bucket.arn}/*"
-        Action   = [
+        Action = [
           "s3:PutObject",
           "s3:PutObjectAcl",
           "s3:GetObject",
@@ -35,4 +26,38 @@ resource "aws_iam_policy" "upload_user_policy" {
       }
     ]
   })
+}
+
+resource "aws_iam_role_policy_attachment" "upload_role_attachment" {
+  policy_arn = aws_iam_policy.upload_role_policy.arn
+  role       = aws_iam_role.frontend_upload_role.name
+}
+
+data "aws_iam_openid_connect_provider" "example" {
+  url = "https://token.actions.githubusercontent.com"
+}
+
+resource "aws_iam_role" "frontend_upload_role" {
+  name = "${aws_s3_bucket.frontend_s3_bucket.bucket}-upload-role"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = "sts:AssumeRoleWithWebIdentity"
+        Principal = {
+          Federated = data.aws_iam_openid_connect_provider.example.arn
+        }
+        Condition = {
+          StringEquals = {
+            "token.actions.githubusercontent.com:aud" = "sts.amazonaws.com"
+          }
+          StringLike = {
+            "token.actions.githubusercontent.com:sub" = "repo:${var.frontend_workflow_repo}:*"
+          }
+        }
+      }
+    ]
+  })
+  force_detach_policies = true
 }
