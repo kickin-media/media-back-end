95393457b5df188226e6d72122a23135fc9071b0
Jeffrey Bakker <jeffrey.bakker@gmail.com>
2026-01-12 20:39:02 +0100

    chore: update uvicorn config to (hopefully) be more stable

--- src/start.sh
+++ src/start.sh
@@ -6,4 +6,11 @@ echo "Performing database migrations..."
 alembic upgrade head
 
 echo "Starting API..."
-uvicorn main:api --host 0.0.0.0 --port 80
+# --limit-max-requests 1000    Restart worker every 1k requests (prevents crashes due to memory leaks)
+# --workers 2                  Multiple workers so that one is always online if the other one is restarting
+uvicorn \
+  --host 0.0.0.0 \
+  --port 80 \
+  --limit-max-requests 1000 \
+  --workers 2 \
+  main:api
