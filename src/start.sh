4134908b82683261a4fa2a5fdabae22bd1c9ef3c
Jonathan Juursema <jonathan.juursema@nedap.com>
2026-05-17 13:20:24 +0200

    Update scaling.

--- src/start.sh
+++ src/start.sh
@@ -7,10 +7,11 @@ alembic upgrade head
 
 echo "Starting API..."
 # --limit-max-requests 1000    Restart worker every 1k requests (prevents crashes due to memory leaks)
-# --workers 2                  Multiple workers so that one is always online if the other one is restarting
+# --workers                    Multiple workers so that one is always online if the other one is restarting
+#                              Default: 5 (2*CPUs+1 for 2-core VM), override via WEB_CONCURRENCY env var
 uvicorn \
   --host 0.0.0.0 \
   --port 80 \
   --limit-max-requests 1000 \
-  --workers 2 \
+  --workers "${WEB_CONCURRENCY:-5}" \
   main:api
